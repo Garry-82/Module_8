66f6b78eb41fc10384583ef73577689caafb5db5
Garry-82 <168720206+Garry-82@users.noreply.github.com>
2024-07-30 23:11:16 +0300

    Add files via upload

--- HW1.py
+++ HW1.py
@@ -1,13 +1,13 @@
 def add_everything_up(a, b):
     try:
-        a + b
         return a + b
     except TypeError as TE:
         return  str(a) + str(b)
 
 
 
-print(add_everything_up(44,'dfgdfg'))
+print(add_everything_up(44,66))
+print(add_everything_up(44,"jgjhgjh"))
 
 
 
